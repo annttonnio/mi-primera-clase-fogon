ac51310460fb59ee7eed698ce4776881306d7edf
annttonnio <antonio56.lop@gmail.com>
2021-11-12 09:24:52 +0100

    02 correcciones miguel

--- Fogon.java
+++ Fogon.java
@@ -1,48 +1,49 @@
 public class Fogon {
     //quien cocina
-    private String Cocinero;
+    private String cocinero;
     //grados del fogon
     private int grados;
     //fogon encendido o no
     private boolean encendido;
     
-    public Fogon (String cocineroActual, boolean encenderTrueFalse){
-        Cocinero = cocineroActual;
+    public Fogon(String cocineroActual, boolean encenderTrueFalse){
+        cocinero = cocineroActual;
         grados = 0;
         encendido = encenderTrueFalse; 
     }
        
     public void encenderFogon(){
-        encendido = true;
+        if (encendido == false){
+            encendido = true;
+        }
+        else {
+            encendido = false;
+            grados = 0;
+        }
+        
     }
     
-    public void apagarFogon(){
-        encendido = false;
-        grados = 0;
-    }
-    
-    public boolean encendido () {
+        public boolean encendido() {
         return encendido;
     }
     
-    public void setGrados(int gradosDelFogon){
-        grados = gradosDelFogon;
+    public void ajustarGrados(int gradosDelFogon){ 
+            grados = grados + gradosDelFogon;  
     }
     
     public void setCocinero(String cocineroDelFogon){
-        Cocinero = cocineroDelFogon;
+        cocinero = cocineroDelFogon;
     }
 
-    public void imprimeDatos () {
-        System.out.println (" Cocinero " + (Cocinero) +". "+ " Temperatura del fogón " + (grados)
+    public void imprimeDatos() {
+        System.out.println (" Cocinero " + (cocinero) +". "+ " Temperatura del fogón " + (grados)
         + " grados. " + "Fogon encendido: " + (encendido)   );
     }
      
-    public String returnDatos () {
+    public String returnDatos() {
         String estadoFogon;
-            estadoFogon = ("Cocinero " + (Cocinero) + ". " + " Temperatura del fogón " + (grados)
-        + " grados. " + " Fogon encendido: " + (encendido)  ); 
-        return estadoFogon; 
+            estadoFogon = ("Cocinero " + (cocinero) + ". " + " Temperatura del fogón " + (grados)+ " grados. " + " Fogon encendido: " + (encendido)  ); 
+            return estadoFogon; 
         
     }
     
